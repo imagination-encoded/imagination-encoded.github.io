655a6ed57e7cb8aaef032ee35a87dbd0369601db
morgengramlich <danila.sergeyev@gmail.com>
2023-11-21 14:13:45 -0600

    Rename static files

---
+++ _app/immutable/chunks/navigation.6707c772.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.bfde1e54.js";const e=o("goto");export{e as g};
--- _app/immutable/chunks/store.d821dfb9.js
+++
@@ -1,1 +0,0 @@
-import{w as s}from"./singletons.591dab51.js";function u(n){return(n==null?void 0:n.length)!==void 0?n:Array.from(n)}const m=s("");export{u as e,m as v};
