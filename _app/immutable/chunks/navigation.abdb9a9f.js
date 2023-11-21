25b5664565b85fa3b2ee05e6f9620782874edff2
morgengramlich <danila.sergeyev@gmail.com>
2023-11-21 14:04:26 -0600

    Rewrite galleries loading

---
+++ _app/immutable/chunks/navigation.abdb9a9f.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.591dab51.js";const e=o("goto");export{e as g};
--- _app/immutable/chunks/store.83b049a7.js
+++
@@ -1,1 +0,0 @@
-import{w as s}from"./singletons.c9e46298.js";function u(n){return(n==null?void 0:n.length)!==void 0?n:Array.from(n)}const c=s({}),g=s("");export{u as e,c as g,g as v};
