96296b997f48cb1e28e3915ae63c3773745e6a40
morgengramlich <danila.sergeyev@gmail.com>
2023-11-24 14:15:16 -0600

    Rewrite manifesto

---
+++ _app/immutable/chunks/navigation.41f3c23a.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.d8c7f12b.js";const e=o("goto");export{e as g};
---
+++ _app/immutable/chunks/store.3c3df136.js
@@ -0,0 +1,1 @@
+import{w as s}from"./singletons.d8c7f12b.js";function u(n){return(n==null?void 0:n.length)!==void 0?n:Array.from(n)}const m=s("");export{u as e,m as v};
